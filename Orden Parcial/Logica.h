1d49982b1f263a3bf20ced43f63d524d95d9590e
xLvkie <bookielyvit@gmail.com>
2025-04-26 13:56:37 -0500

    Orden Parcial Parte 3

--- Orden Parcial/Logica.h
+++ Orden Parcial/Logica.h
@@ -5,38 +5,43 @@
 #include <vector>
 #include <map>
 #include <string>
-#include <algorithm>
 #include <random>
-#include <ctime>
 
 using namespace std;
 
 void generarMallaCurricular() {
-    //las constantes no cambian con el tiempo a comparación de las varibles
+    //las constantes no cambian
     const int totalCursos = 50;
     const int totalCiclos = 10;
     const int cursosPorCiclo = 5;
-    vector<string> cursos; //arreglo
-    map<string, vector<string>> prerrequisitos; //diccionario
-    vector<vector<string>> niveles; //arreglo de arreglos
+    vector<string> cursos; //vector de cursos {"c1", "c2", "c3", ..., "c50"}
+    map<string, vector<string>> prerrequisitos; //estructura que guarda pares clave-valor p[clave] = {valor}
+    vector<vector<string>> niveles;//vector de vectores tipo strings n = { {"1" , "2"} , {...} ... {...} }
     int indiceCurso = 0;
 
     // Generar los 50 cursos [C1; C50]
     for (int i = 1; i <= totalCursos; ++i) {
-        cursos.push_back("C" + to_string(i));
+        cursos.push_back("C" + to_string(i)); //mandamos datos al vector cursos 
     }
 
-    // Semilla de tiempo, srand((unsigned)time(0)); es otra forma de escribirlo
-    srand(time(NULL));
-
+    // ----
     // Asigna los cursos a los ciclos y prerrequisitos básicos
     for (int ciclo = 1; ciclo <= totalCiclos; ++ciclo) {
         vector<string> cicloCursos;
-        for (int j = 0; j < cursosPorCiclo; ++j) {
-            if (indiceCurso < totalCursos) {
-                string curso = cursos[indiceCurso];
-                cicloCursos.push_back(curso);
+        for (int j = 0; j < cursosPorCiclo; ++j) { 
+            if (indiceCurso < totalCursos) { // index < 50
+                string curso = cursos[indiceCurso]; //c1, c2 ...
+                cicloCursos.push_back(curso); // {"c1" , "c2" ... "c5"
                 //Omitimos el ciclo 1, ya que los cursos C1, C2, C3, C4 y C5 no tiene prerrequisitos
+
+                /*
+           
+                 Este apartado es donde se da la relación reflexiva aRa
+                 prereequisitos["cn"] = {"cn"}; --> aRa
+                 solo que en este caso omitimos los cursos el 1er ciclo
+
+                */
+
                 if (ciclo != 1)
                     prerrequisitos[curso] = { curso };
                 else
@@ -45,17 +50,51 @@ void generarMallaCurricular() {
                 ++indiceCurso;
             }
         }
+        /*
+
+         Agregamos al vector de vectores 'niveles' el vector 'cicloCursos'
+         hasta su ultima actualización del bucle for numero 2, luego volvemos
+         al primer for donde su inicialización lo incrementamos en 1 y se vuelve
+         a inicializar el vector cicloCursos.
+
+        */
+
         niveles.push_back(cicloCursos);
     }
 
+    // ----
     // Asigna prerrequisitos para los cursos C6 - C50
-    for (int ciclo = 1; ciclo < totalCiclos; ++ciclo) {
+    for (int ciclo = 1; ciclo < totalCiclos; ++ciclo) { //omitimos el ciclo 1 ya que este no cuenta con prerrequisitos
         for (const string& curso : niveles[ciclo]) {
-            const vector<string>& candidatos = niveles[ciclo - 1];
-            int prerreqCount = rand() % 3;
-            vector<string> seleccionados = candidatos;
-            random_shuffle(seleccionados.begin(), seleccionados.end());
-            seleccionados.resize(min(prerreqCount, (int)seleccionados.size()));
+            /*
+            
+             Lo que hace este for() es recorrer cada curso uno por uno de
+             los cursos del nivel[ciclo], asi hasta que se cambie el valor de ciclo
+
+             niveles[1] = {c6, c7 ... c10}
+            
+            */
+
+            const vector<string>& candidatos = niveles[ciclo - 1]; //candidatos para ser prerrequisitos de cursos posteriores
+            int prerreqCount = rand() % 3; // 0, 1, 3
+            vector<string> seleccionados = candidatos; //copia canditados a seleccionados
+            random_shuffle(seleccionados.begin(), seleccionados.end()); //mezcla aleatoriamente el vector seleccionados de inicio(begin) a fin(end)
+            seleccionados.resize(min(prerreqCount, (int)seleccionados.size())); //cambia el tamaño del vector seleccionados en base a 'prerreqCount' y el tamaño del vector
+
+            /*
+            
+             Agregamos los prerrequisitos del 'curso' (2do for) a la
+             estructura clave-valor 'prerrequisitos'
+            
+             Por ejemplos sea curso = "c6" (esto seria su clave tipo string)
+             prerrequisitos["c6"].insert(
+             prerrequisitos["c6"].end(), -> posición en la cual agregamos los valores, siendo este caso el final de map
+                seleccionados.begin(), -> inicio del Dominio de seleccionados (desde que elemento)
+                seleccionados.end() -> fin del Dominio de seleccionaods (hasta que elemento)
+             )
+            
+            */
+
 
             prerrequisitos[curso].insert(
                 prerrequisitos[curso].end(),
@@ -65,19 +104,53 @@ void generarMallaCurricular() {
         }
     }
 
+    // ----
     // Asigna aleatoriamente que los cursos C1 - C45 sean prerrequisitos de 0, 1 o 2 cursos
-    for (int ciclo = 0; ciclo < totalCiclos - 1; ++ciclo) {
+    for (int ciclo = 0; ciclo < totalCiclos - 1; ++ciclo) { //tomamdos todos los ciclos, salvo el ultimo ya que estos no pueden ser prerrequisitos de otros cursos
         for (const string& curso : niveles[ciclo]) {
-            int puedeSerPrerrequisito = rand() % 3;
+
+            /*
+
+             Lo que hace este for() es recorrer cada curso uno por uno de
+             los cursos del nivel[ciclo], asi hasta que se cambie el valor de ciclo
+
+             niveles[0] = {c1, c2 ... c5}
+
+           */
+
+            int puedeSerPrerrequisito = rand() % 3; // 0, 1, 3
+            /*
+             
+             Digamos que 'curso' = "c1", if (puedeSerPrerrequisito > 0)
+             quiere decir que, sí 'puedeSerPrerrequisito' es mayor a 0, entonces
+             "c1" (curso) sera prerrequisto de otros 'int puedeSerPrerrequisito'
+             por ejemplo: c1 es prerrequisto de c7, c9, etc.
+            
+            */
+
             if (puedeSerPrerrequisito > 0) {
+
+                /*
+                 
+                 Buscamos candidatos a "c1" para que sea prerrequisito en cursos posteriores
+                 para ello en el vector candidatos insertamos cursos de ciclos posteriores 
+                 por ejemplo si estamos en el ciclo 1, entonces buscara candidatos de ciclos posteriores
+                 (ciclo 2 hasta el ciclo 10)
+                
+                */
+
+
                 vector<string> candidatos;
                 for (int siguienteCiclo = ciclo + 1; siguienteCiclo < totalCiclos; ++siguienteCiclo) {
                     candidatos.insert(candidatos.end(), niveles[siguienteCiclo].begin(), niveles[siguienteCiclo].end());
                 }
 
+                //una vez el vector candidatos obtenga todos sus valores los mezclamos aleatoriamente (random_shuffle)
                 random_shuffle(candidatos.begin(), candidatos.end());
+                //luego escogemos tantos cursos podamos según el valor de 'puedeSerPrerrequisito'
                 vector<string> asignados(candidatos.begin(), candidatos.begin() + min(puedeSerPrerrequisito, (int)candidatos.size()));
 
+                //finalmente asignamos mediante el vector asignados como prerrequsitos
                 for (const string& c : asignados) {
                     prerrequisitos[c].push_back(curso);
                 }
@@ -85,6 +158,8 @@ void generarMallaCurricular() {
         }
     }
 
+    // ---
+    // Imprimimos por consola
     system("cls");
     cout << "Orden de cursos por ciclo y sus prerrequisitos:\n\n";
     for (int i = 0; i < niveles.size(); ++i) {
@@ -97,9 +172,6 @@ void generarMallaCurricular() {
                 if (j < prereqs.size() - 1) cout << ", ";
             }
             cout << ")\n";
-            if (prerrequisitos[curso].size() == 1 && i > 0) {
-                cout << "SOLO UN CURSO\n";
-            }
         }
         cout << endl;
     }
--- Orden Parcial/Source.cpp
+++ Orden Parcial/Source.cpp
@@ -2,13 +2,14 @@
 #include "Logica.h"
 
 int main() {
+    srand(time(NULL));
     int opcion;
 
     do {
         mostrarMenu();
         cin >> opcion;
 
-        cin.ignore(); // limpiar buffer
+        cin.ignore();
 
         switch (opcion) {
         case 1:
@@ -27,17 +28,25 @@ int main() {
             cin.get();
             break;
         }
-    } while (opcion > 4 && opcion < 0);
+    } while (opcion != 4);
 
     return 0;
 }
 
 /*
 
+NOTA 1:
 Pienso integrar una API para obtener cursos universitarios reales, es por
 eso que agregue más mallas curriculares de carrera, pero veremos si es factible
 mi pequeña ideita waza 
 
 Bookie-
+
+NOTA 2:
+Cuando se genera una malla curricular sera tal que asi
+Ciclo 7:
+C31 (Prerrequisitos: C31, C29, ETC)
+para leerlo desde una anotación matematico es de la forma
+(C29, C31) C29 esta relacionado con C31 -> c29 R c31
 
 */
